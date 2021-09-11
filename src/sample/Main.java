baabb12a11390ee28c6db0a756cbc632f30ada3e
andresherrera9dev <79882149+andresherrera9dev@users.noreply.github.com>
2021-09-10 23:07:56 -0500

    Avance para agreagar eventos (Beta)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,40 +12,38 @@ import javafx.stage.Stage;
 
 import java.util.Random;
 
+
 public class Main extends Application {
-
-    static Scene scene;
-    Button btn1;
-    Button btn2;
-    Button btn3;
-
-    HBox hbx;
-    VBox vbx;
-    BorderPane pane;
-    FlowPane flow;
-    GridPane grid;
 
     public class Containers {
 
         public VBox init_Vbox() {
+            VBox vbx;
+            Button btn1;
+            Button btn2;
+            Button btn3;
             btn1 = new Button("Boton 1");
             btn2 = new Button("Boton 2");
             btn3 = new Button("Boton 3");
             vbx = new VBox();
             vbx.setSpacing(10);
             vbx.setPadding(new Insets(20));
-            hbx.getChildren().addAll(btn1, btn2, btn3);
+            vbx.getChildren().addAll(btn1, btn2, btn3);
             return vbx;
         }
 
         public HBox init_Hbox() {
+            Button btn1;
+            Button btn2;
+            Button btn3;
+            HBox hbx;
             btn1 = new Button("Boton 1");
             btn2 = new Button("Boton 2");
             btn3 = new Button("Boton 3");
             hbx = new HBox();
             hbx.setSpacing(10);
             hbx.setPadding(new Insets(20));
-            vbx.getChildren().addAll(btn1, btn2, btn3);
+            hbx.getChildren().addAll(btn1, btn2, btn3);
             return hbx;
         }
 
@@ -66,6 +64,7 @@ public class Main extends Application {
         }
 
         public FlowPane init_Flowpane(boolean vertical) {
+            FlowPane flow;
             if (vertical) {
                 flow = new FlowPane(Orientation.VERTICAL);
                 flow.setVgap(10);
@@ -160,6 +159,24 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Cuadro_magico_v2 jugar = new Cuadro_magico_v2();
+        GridPane grid = new GridPane();
+        BorderPane pane = new BorderPane();
+        Label lbl = new Label("Cuadro Magico");
+
+        HBox hbx = new HBox();
+        Button btn1 = new Button("Cuadro Magico");
+        hbx.setSpacing(10);
+        hbx.setPadding(new Insets(20));
+        hbx.getChildren().addAll(btn1);
+        hbx.setAlignment(Pos.CENTER);
+
+        HBox hbx2 = new HBox();
+        Button btn2 = new Button("-Reducir");
+        Button btn3 = new Button("+Aumentar");
+        hbx2.setSpacing(10);
+        hbx2.setPadding(new Insets(20));
+        hbx2.getChildren().addAll(btn3,btn2);
+        hbx2.setAlignment(Pos.CENTER);
 
         int dimension = 3;
         jugar.iniciar(dimension);
@@ -177,9 +194,14 @@ public class Main extends Application {
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPrefWidth(800);
-
         grid.setAlignment(Pos.CENTER);
-        Scene scene = new Scene(grid, 300, 500);
+
+        pane.setTop(hbx);
+        pane.setCenter(grid);
+        pane.setBottom(hbx2);
+        pane.setPadding(new Insets(5, 5, 5, 5));
+
+        Scene scene = new Scene(pane, 200, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
